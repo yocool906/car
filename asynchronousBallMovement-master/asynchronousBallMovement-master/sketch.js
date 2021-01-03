db34932d6fb7e13027dfd9af9014d85eb6e216d3
yocool906 <74095298+yocool906@users.noreply.github.com>
2021-01-03 07:53:11 -0500

    Add files via upload

---
+++ asynchronousBallMovement-master/asynchronousBallMovement-master/sketch.js
@@ -0,0 +1,40 @@
+var ball;
+var database
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database()
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    var ballpos = database.ref("ball/position")
+    ballpos.on("value",readposition)
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function readposition(data){
+    position=data.val()
+    ball.x = position.x
+    ball.y = position.y
+}
+function writePosition(x,y){
+    database.ref('ball/position').set({
+        'x':position.x+x,
+        'y':position.y+y
+        
+    })
+}
